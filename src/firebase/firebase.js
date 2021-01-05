cffcf7c6967a1be77e9efdc5b105d4a3faf2853f
rafael <rfalconi@unibe.edu.ec>
2021-01-05 11:45:12 -0500

    integración de firebase con react native para creación de registro y login.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+var firebaseConfig = {
+    apiKey: "AIzaSyC6YYAzhxuo-aNf-tuCHIwQho7n6Hi5dKk",
+    authDomain: "react-native-552ce.firebaseapp.com",
+    projectId: "react-native-552ce",
+    storageBucket: "react-native-552ce.appspot.com",
+    messagingSenderId: "1000178437640",
+    appId: "1:1000178437640:web:5abbe41a74efdec67d4bbd",
+    measurementId: "G-RSRDJYT6BT"
+};
+export default  firebase.initializeApp(firebaseConfig);
--- src/screens/drawernavigation.js
+++ src/screens/drawernavigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import ImageCarousel from "../components/imageCarousel";
 import ImageComponent from "../components/image";
 
 const Drawer = createDrawerNavigator();
@@ -10,7 +9,6 @@ export default function DrawerScreen() {
     return (
 
         <Drawer.Navigator>
-            <Drawer.Screen name="principal" component={ImageCarousel}/>
             <Drawer.Screen name="secundaria" component={ImageComponent}/>
         </Drawer.Navigator>
 
