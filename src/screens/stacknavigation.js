b75d6c256eac96cdacc791e22357b72d141252e1
rafael <rfalconi@unibe.edu.ec>
2021-01-05 11:53:31 -0500

    buscar todo en una colecion

--- src/screens/stacknavigation.js
+++ src/screens/stacknavigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 import ListPage from '../Lista/lista';
 import Item from "../Lista/item";
-
+import firebase from '../firebase/firebase';
+import "firebase/firestore";
 const Stack = createStackNavigator();
 
 
 export default function Principal() {
+    firebase.firestore().collection('user').get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data());
+            });
+        });
     return (
 
         <Stack.Navigator>
@@ -20,8 +27,8 @@ export default function Principal() {
                 headerTitleStyle: {
                     fontWeight: 'bold',
                 },
-            }}/>
-            <Stack.Screen name="item" component={Item}/>
+            }} />
+            <Stack.Screen name="item" component={Item} />
 
         </Stack.Navigator>
 
