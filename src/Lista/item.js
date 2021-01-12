c32478f0ee2ce155d45365456f384441413789db
rafael <rfalconi@unibe.edu.ec>
2021-01-12 10:30:54 -0500

    obtener uno y todos los documentos de firebase, componente de cargando y modal.

--- src/Lista/item.js
+++ src/Lista/item.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Dimensions, Image, StyleSheet, Text, View} from "react-native";
 import {useRoute} from '@react-navigation/native';
+
 const {width: screenWidth} = Dimensions.get('window')
 const {height: screenHeight} = Dimensions.get('window')
+import firebase from "../firebase/firebase";
+import "firebase/firestore";
+
 export default function Item() {
-
+    const framework = firebase.firestore().collection('framework');
+    const [item, setItem] = useState({});
+    const [isLoading, setLoading] = useState(true);
     let route = useRoute();
-    let item = route.params.texto;
+    let id = route.params.id;
+    const synch = () => {
+        framework.doc(id).get().then((snapshot) => {
 
+            let frameworkFromFirebase = {
+                id: snapshot.id,
+                name: snapshot.data().name,
+                photo: snapshot.data().photo
+            }
+            setItem(frameworkFromFirebase);
+        }).finally(() => setLoading(false));
+    }
+    useEffect(() => {
+        let isMounted = true;
+        synch();
+
+        return () => {
+            isMounted = false
+        };
+    }, [])
     return (
         <View style={styles.item}>
             <View style={styles.containerImage}>
@@ -29,12 +53,12 @@ const styles = StyleSheet.create({
     containerImage: {
         flex: 1,
         margin: 10,
-        textAlign:'center'
+        textAlign: 'center'
     },
     image: {
 
-        width: screenWidth-150,
-        height: screenHeight-150
+        width: screenWidth - 150,
+        height: screenHeight - 150
     },
     containerInfo: {
         flex: 5,
